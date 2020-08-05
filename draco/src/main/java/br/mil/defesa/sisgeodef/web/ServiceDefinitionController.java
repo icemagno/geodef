af91ad7bf8d710d2c6f72a14e4a08d24fd4041d2
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2020-08-05 00:42:42 -0300

    Adicionado DRACO

---
+++ draco/src/main/java/br/mil/defesa/sisgeodef/web/ServiceDefinitionController.java
@@ -0,0 +1,23 @@
+package br.mil.defesa.sisgeodef.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.mil.defesa.sisgeodef.config.swagger.ServiceDefinitionsContext;
+
+
+@RestController
+public class ServiceDefinitionController {
+	
+	@Autowired
+	private ServiceDefinitionsContext definitionContext;
+	
+	@GetMapping("/service/{servicename}")
+	public String getServiceDefinition(@PathVariable("servicename") String serviceName){
+		
+		return definitionContext.getSwaggerDefinition(serviceName);
+		
+	}
+}
