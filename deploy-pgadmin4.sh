a7ffbe170aff7f485cecbe2416a9935ac0ec5fa9
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2020-08-19 22:15:28 -0300

    Versão do PGADMIN sobe para a última disponível

--- deploy-pgadmin4.sh
+++ deploy-pgadmin4.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-svn update
-
 mkdir /srv/pgadmin/
 cd pgadmin
+
+svn update
 
 docker ps -a | awk '{ print $1,$2 }' | grep sisgeodef/pgadmin | awk '{print $1 }' | xargs -I {} docker rm -f {}
 docker rmi sisgeodef/pgadmin
@@ -14,7 +14,6 @@ docker run --name pgadmin --hostname=pgadmin \
 -e PGADMIN_DEFAULT_PASSWORD=sisgeodef \
 -v /etc/localtime:/etc/localtime:ro \
 -p 36321:80 \
--v /srv/pgadmin/:/var/lib/pgadmin \
 -d sisgeodef/pgadmin	
 
 docker network connect apolo pgadmin
