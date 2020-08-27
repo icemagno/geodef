e59f97b8bb352aadf159dbdf6aa465dee9ee5cfc
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2020-08-27 02:59:06 -0300

    Novo OLIMPO

--- deploy-olimpo.sh
+++ deploy-olimpo.sh
@@ -1,21 +1,22 @@
 #! /bin/sh
 
-cd olimpo
+cd olimpo/
 svn update
+mvn clean package
 
 docker ps -a | awk '{ print $1,$2 }' | grep sisgeodef/olimpo:1.0 | awk '{print $1 }' | xargs -I {} docker rm -f {}
 docker rmi sisgeodef/olimpo:1.0
 
 docker build --tag=sisgeodef/olimpo:1.0 --rm=true .
 
-docker run --name olimpo --hostname=olimpo  \
--v /etc/localtime:/etc/localtime:ro \
--v /srv/olimpo/:/data/ \
--e SERVE_STATIC=0 \
--e LOG_LEVEL=debug \
--p 36503:8000 \
--d sisgeodef/olimpo:1.0
+docker run --name olimpo --hostname=olimpo \
+	-e ARCHIMEDES_CONFIG_URI=http://archimedes:36206/ \
+	-e CONFIG_PROFILES=default \
+	-v /srv/olimpo/:/data \
+	-v /etc/localtime:/etc/localtime:ro \
+	-p 36503:36503 \
+	-d sisgeodef/olimpo:1.0	
 
-docker network connect apolo olimpo
 docker network connect sisgeodef olimpo
+docker network connect apolo olimpo
 
