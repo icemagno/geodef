d71efc8b4813436bf825f9f9d30c22439a275858
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2020-09-07 20:59:51 -0300

    Testes dos solucionadores

---
+++ solucionadores/testes/test-rota.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+wget "http://localhost:36305/route?point=-22.863522%2C-43.566971&point=-22.912231%2C-43.072586&locale=pt-BR&vehicle=car&weighting=fastest&elevation=false&use_miles=false&layer=OpenStreetMap&points_encoded=false" -O rota.json 
