fb5dfcde769a5797956484ff271156a5e297ea9c
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2020-12-01 01:00:18 -0300

    Retirado o guia rápido de dentro do Atlas. Foi para o MIDAS.

--- atlas/src/main/webapp/resources/img/fastguide/zoom_pageinfo.js
+++
@@ -1,2 +0,0 @@
-pageinfo = [[0,0,0,0,null]];
-pagedata = [ ["bem_vindo_a_ajuda.htm","Guia Rápido","Ajuda do Geoportal | SisGEODEF Barra de Camadas - Permite habilitar/desabilitar a visualização da barra de &#8220;Adicionar Camada&#8221; e &#8220;Camadas Ativas/Ce...",""]];
